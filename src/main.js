e45b5604c5a2159d1f2bc30fd6f232b3b4194c28
Saurabh Yadav <sy5605781@gmail.com>
2026-05-19 23:58:11 +0530

    callback 'on' related properties

--- src/main.js
+++ src/main.js
@@ -15,17 +15,35 @@ import "./style.css";
 //   stagger: -0.3
 // })
 
-// For the arrow type animations we can use stagger with object values  // center, edges, start, end, random
-gsap.to('.box', {
-  x:1200,
+// For various types of animations we can use stagger with object values  // center, edges, start, end, random
+// gsap.to('.box', {
+//   x:1200,
+//   duration: 1.5,
+//   delay: 1,
+//   repeat: 4,
+//   yoyo: true,
+//   repeatDelay: 1,
+//   backgroundColor: 'green',
+//   stagger: {
+//     each: 0.2,
+//     from: "center"
+//   }
+// })
+
+
+// The on-related properties are essentially callbacks. They’re super handy for triggering custom logic when animations start, update, or complete.
+// like onStart → Fires when the tween/timeline begins.
+// onUpdate → Fires every time the tween updates (e.g., on each frame).
+// onComplete → Fires once the tween finishes.
+// onRepeat → Fires each time a repeat cycle begins.
+// onReverseComplete → Fires when a reversed tween finishes going back to its start.
+gsap.to(".box", {
+  x:500,
   duration: 1.5,
   delay: 1,
-  repeat: 4,
-  yoyo: true,
-  repeatDelay: 1,
-  backgroundColor: 'green',
-  stagger: {
-    each: 0.2,
-    from: "random"
+  backgroundColor: 'blue',
+  repeat: 1,
+  onComplete: ()=> {
+    console.log("Animation end")
   }
 })
